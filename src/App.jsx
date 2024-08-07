aee51bd7c807654a323121215dda83e6759b3fda
leoseis <eboscrib2@yahoo.com>
2024-08-07 14:09:13 +0100

    filter component added code from refrence

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
+
 import React from 'react'
 import NavBar from './components/NavBar'
+import Filter from './components/Filter'
+
 
 const App = () => {
   return (
     <div>
     <NavBar/>
+    <Filter />
     </div>
   )
 }
