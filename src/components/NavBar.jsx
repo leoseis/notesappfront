7d0453a30205cb4db1b50e52b0a41d26f5c82b27
leoseis <eboscrib2@yahoo.com>
2024-08-07 11:32:31 +0100

    added nav bar with labels

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,46 @@
+import React from 'react'
+import {Navbar,Nav,Container,Row,NavDropdown} from 'react-bootstrap'
+import { FaSquarePlus } from "react-icons/fa6";
+import {  Form, FormControl, Button } from 'react-bootstrap';
+
+
+const NavBar = () => {
+  return (
+    <div>
+      <Navbar expand="lg" className="bg-body-tertiary">
+      <Container fluid>
+        <Navbar.Brand href="#">LEO's Page </Navbar.Brand>
+        <Button variant="outline-light">Light</Button>
+        <Navbar.Toggle aria-controls="navbarScroll" />
+        <Navbar.Collapse id="navbarScroll">
+          <Nav
+            className="me-auto my-2 my-lg-0"
+            style={{ maxHeight: '100px' }}
+            navbarScroll
+          >
+            <button
+            className="btn btn-outline-primary btn-md"
+            type="button"
+           
+          >
+            <FaSquarePlus className="me-2 fs-6" /> Add Notes
+          </button>
+
+          </Nav>
+          <Form className="d-flex">
+            <Form.Control
+              type="search"
+              placeholder="Search"
+              className="me-2"
+              aria-label="Search"
+            />
+            <Button variant="outline-success">Search</Button>
+          </Form>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+    </div>
+  )
+}
+
+export default NavBar
