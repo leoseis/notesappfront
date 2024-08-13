c0fdae0529e9070931d0bf63e00de4757fbaca43
leoseis <eboscrib2@yahoo.com>
2024-08-13 11:16:20 +0100

    frontpage created for home at port 3000

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,16 @@
-
-import React from 'react'
-import NavBar from './components/NavBar'
-import Filter from './components/Filter'
-import NoteCardContainer from './components/NoteCardContainer'
-
+import React from 'react';
+import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom';
+import Home from './pages/Home';
 
 const App = () => {
-  return (
-    <div>
-    <NavBar/>
-    <Filter />
-    <NoteCardContainer/>
-    </div>
-  )
+
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path="/" element={<Home />} />
+    )
+  );
+
+  return <RouterProvider router={router} />;
 }
 
-export default App
+export default App;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Filter from '../components/Filter'
+import NoteCardContainer from '../components/NoteCardContainer'
+
+const Home = () => {
+  return (
+    <>           
+     <Filter />
+     <NoteCardContainer/>
+    </>
+  )
+}
+
+export default Home
