c9096dfafed14e7f5118e1328f80a607f648cf45
leoseis <eboscrib2@yahoo.com>
2024-08-15 11:33:45 +0100

    Outlet layout created so the Nav component can appear in all pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom';
 import Home from './pages/Home';
+import MainLayout from './layouts/MainLayout';
 
 const App = () => {
 
   const router = createBrowserRouter(
     createRoutesFromElements(
-      <Route path="/" element={<Home />} />
+      <Route path = '/' element ={<MainLayout/>}>
+         <Route path="/" element={<Home />} />
+      </Route>
+     
     )
   );
 
