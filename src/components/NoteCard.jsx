c9cac91c70639d7d36271669f2930a42b789a6da
leoseis <eboscrib2@yahoo.com>
2024-09-04 17:55:03 +0100

    addaed cont variable to reduce number of body to twenty xters and added another const for changing category to rep diif colors

--- src/components/NoteCard.jsx
+++ src/components/NoteCard.jsx
@@ -4,11 +4,16 @@ import { FaNoteSticky } from "react-icons/fa6";
 import { Link } from 'react-router-dom';
 
 const NoteCard = ({note}) => {
+
+  const body = `${note.body.split(" ").slice(0, 20).join(" ")} ...`
+  const color = note.category == "BUSINESS" ? "blue" : note.category == "PERSONAL" ? "green" : "purple"
+
+
   return (
     <div className="col-md-4 single-note-item all-category">
       <div className="card card-body">
-        <span className="side-stick" style={{ backgroundColor: 'blue' }}></span>
-        <FaNoteSticky style={{ marginLeft: "auto", color: 'blue' }} />
+        <span className="side-stick" style={{ backgroundColor: color }}></span>
+        <FaNoteSticky style={{ marginLeft: "auto", color: color }} />
         <Link to="/note-detail" style={{ textDecoration: "none", color: "black" }}>
           <h5 className="note-title text-truncate w-75 mb-0">
             {note.title}
@@ -17,13 +22,13 @@ const NoteCard = ({note}) => {
         <p className="note-date font-12 text-muted">11 March</p>
         <div className="note-content">
           <p className="note-inner-content text-muted">
-            Blandit tempus porttitor aasfs. Integer posuere erat a ante venenatis.
+          {body}
           </p>
         </div>
         <div className="d-flex align-items-center">
           <span className="d-flex justify-content-around">
             <Link to="/notes-detail">
-              <MdMarkunread style={{ fontSize: "25px", cursor: "pointer", color: 'blue' }} />
+              <MdMarkunread style={{ fontSize: "25px", cursor: "pointer", color: color }} />
             </Link>
             <small className="text-muted">{note.category}</small>
           </span>
