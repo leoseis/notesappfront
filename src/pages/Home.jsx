75e21d174fb0ca71774b3efff765f202648894f5
leoseis <eboscrib2@yahoo.com>
2024-11-06 09:23:36 +0100

    if text is less than 1 display nothing

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,14 @@
-import React from 'react'
-import Filter from '../components/Filter'
-import NoteCardContainer from '../components/NoteCardContainer'
+import React from 'react';
+import Filter from '../components/Filter';
+import NoteCardContainer from '../components/NoteCardContainer';
 
-const Home = ({notes,loading, handleFilterText,filterText}) => {
+const Home = ({ notes, loading, handleFilterText }) => {
   return (
     <> 
-    {filterText}          
-     <Filter handleFilterText={handleFilterText} />
-     <NoteCardContainer notes={notes}loading={loading}/>
+      {notes.length > 0 && <Filter handleFilterText={handleFilterText} />}
+      <NoteCardContainer notes={notes} loading={loading} />
     </>
-  )
-}
+  );
+};
 
-export default Home  
+export default Home;
